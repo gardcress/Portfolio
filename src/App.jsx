e7ae22afff97c7d5bd27c5afdd58517218001484
Tropaeo <kevinkjager@outlook.com>
2023-01-18 23:29:50 +0100

    test images

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <p>This is my portfolio</p>
+      <img src='https://raw.githubusercontent.com/Tropaeo/portfolio/main/public/favicon.ico'></img>
     </div>
   );
 }
