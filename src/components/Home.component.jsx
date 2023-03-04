0b20e4287c0b2838f051cb616ae2a2690fbbebd1
Tropaeo <kevinkjager@outlook.com>
2023-03-04 18:03:49 +0100

    added skills list

--- src/components/Home.component.jsx
+++ src/components/Home.component.jsx
@@ -1,19 +1,37 @@
 import HeaderTitle from "./HeaderTitle.component";
 
 function Home(props){
-    const lang = props.lang;
+    const lang = props.lang.home;
 
 
     return(
         <div className="home" style={{textAlign: "center"}}>
             <img className="profileImage" src='https://media.licdn.com/dms/image/D4D03AQEkmjvEnpTsrw/profile-displayphoto-shrink_400_400/0/1673964306866?e=1683158400&v=beta&t=9QHBZE6PcfmGHdt5Ubtutwlga-Xx_CuzhsuIRZXE8Xc'></img>
             <div className="homeSiding">
-                <HeaderTitle className="" size="25px" text={lang.home.welcome_title}></HeaderTitle>
+                <HeaderTitle className="" size="25px" text={lang.welcome_title}></HeaderTitle>
             </div>
             
             <p className="homeTitle homeSiding">
-                {lang.home.welcome_desc}
+                {lang.welcome_desc}
             </p>
+
+            <p style={{fontSize: "20px", fontWeight: "bold"}}>
+                {lang.skills}
+            </p>
+            <div style={{width: "fit-content", textAlign:"left", marginLeft:"auto", marginRight: "auto"}}><ul class="check-list">
+                <li>Javascript</li>
+                <li>Html</li>
+                <li>CSS</li>
+                <li>Java</li>
+                <li>C#</li>
+                <li>C</li>
+                <li>Bootstrap</li>
+                <li>Node.js</li>
+                <li>React.js</li>
+                <li>Github (git)</li>
+                <li>...</li>
+            </ul></div>
+
         </div>
     )
 }
--- src/lang.js
+++ src/lang.js
@@ -13,6 +13,7 @@ export function sv(){
         "Jag heter Kevin och på denna hemsida har jag försökt samla och visa upp allt som jag har erfarit och lärt mig när det kommer till teknik och IT. " + 
         "Personligen har jag alltid tyckt om att programmera, lösa problem som uppstår och arbeta med datorer osv. " +
         "Jag tar ofta eget initiativ, är väldigt driven och ser gärna till att arbetet/projektet blir klart i tid som planerat.",
+        skills: "Skickligheter:",
     }
 
     const lang = {
@@ -39,6 +40,8 @@ export function en(){
         "My name is Kevin and on this website I have tried to gather and showcase everything I have experienced and learnt when it comes to technology and IT. " +
         "I personally have always liked to code, solve problems and generally work with computers etc. " + 
         "I oftentimes take initiative, am driven and will gladly see to it that the work/project gets done on time as planned.",
+        skills: "Skills:",
+
     }
 
     const lang = {
